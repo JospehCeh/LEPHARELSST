d44e34a3bb5d712df91e6f50bad8ad39f8c618f4
Joseph Chevalier <joe_ontheflex@hotmail.fr>
2022-04-05 15:11:21 +0200

    runs from notebook

--- runLePhareLSST.sh
+++ runLePhareLSST.sh
@@ -40,11 +40,12 @@ $LEPHAREDIR/source/sedtolib -t G -c LSST.para
 
 ## use the galaxy templates + filters to derive a library of predicted magnitudes and store it in `$LEPHAREWORK/lib_mag` (the parameters correspond to enabling emission lines correlated to UV light + free factor in scaling these lines, mo$
 echo "Magnitudes"
-$LEPHAREDIR/source/mag_gal -t G -c LSST.para 
+$LEPHAREDIR/source/mag_gal -t G -c LSST.para
 
 ## finally proceed to photometric redshift estimation
 echo "Estimation"
 $LEPHAREDIR/source/zphota -c LSST.para -CAT_IN "$CAT_FILE_IN" -CAT_TYPE LONG -CAT_OUT zphot_long.out -AUTO_ADAPT YES
+#$LEPHAREDIR/source/zphota -c LSST_specId.para -CAT_IN "$CAT_FILE_IN" -CAT_TYPE LONG -CAT_OUT zphot_long_specId.out -AUTO_ADAPT YES
 #$LEPHAREDIR/source/zphota -c LSST.para -CAT_IN train_DC2_VALID_CAT_IN.in -CAT_TYPE LONG -CAT_OUT train_zphot_long.out -AUTO_ADAPT YES
 #$LEPHAREDIR/source/zphota -c LSST.para -CAT_IN COSMOS.in -CAT_OUT zphot_short.out -ZPHOTLIB VISTA_COSMOS_FREE,ALLSTAR_COSMOS,QSO_COSMOS -AUTO_ADAPT YES
 
